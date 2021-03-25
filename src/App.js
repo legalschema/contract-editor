ab9f74146711816b34fa7fbb406f24b76733975d
Dan Selman <danscode@selman.org>
2021-03-25 11:18:46 +0000

    update text
    Signed-off-by: Dan Selman <danscode@selman.org>

--- src/App.js
+++ src/App.js
@@ -22,7 +22,10 @@ export const App = () => {
       <h1>Contract Editor Demo</h1>
       <p>
         This is a demo of editing a contract containing a UK Legal Schema <a href="https://templates.legalschema.org/bill-of-lading@0.1.0.html">Bill of Lading template</a>.
-        The contract editor supports editing documents with rich-text formatting and embedded structured templates.
+        The contract editor supports editing documents with rich-text formatting and embedded structured templates. The editor itself is Open Source and may be freely embedded in applications. [Contributions are welcome](https://github.com/accordproject/web-components).
+      </p>
+      <p>
+        The contract editor is Open Source and may be freely embedded in applications. [Contributions are welcome](https://github.com/accordproject/web-components).
       </p>
       <LegalEditor />
     </Wrapper>
